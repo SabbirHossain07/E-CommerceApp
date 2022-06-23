6d9e912b6cc5a539bf2b8c512317304a0953a802
SabbirHossain07 <sabbirsopnil07@gmail.com>
2022-06-23 22:00:14 +0600

    Initial Commit

---
+++ E-CommerceApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  E-CommerceApp
+//
+//  Created by Sopnil Sohan on 23/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ E-CommerceApp/E_CommerceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  E_CommerceAppApp.swift
+//  E-CommerceApp
+//
+//  Created by Sopnil Sohan on 23/6/22.
+//
+
+import SwiftUI
+
+@main
+struct E_CommerceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
